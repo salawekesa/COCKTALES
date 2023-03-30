e6a2191847255f83e43e602d2772ab6e5fe6b95e
salathiel wekesa <107699372+salawekesa@users.noreply.github.com>
2023-03-31 00:05:04 +0300

    Added Landing page

--- Base/models.py
+++ Base/models.py
@@ -1,20 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 
-class Room(models.Model):
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    # avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class Topic(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    updated = models.TimeField(auto_now=True)
-    created = models.TimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-    
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+
+
 class Message(models.Model):
-    # user = 
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)  
-    description = models.TextField
-    updated = models.TimeField(auto_now=True)
-    created = models.TimeField(auto_now_add=True)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body[0:50]
